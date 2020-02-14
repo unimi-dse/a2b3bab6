7ec682e0abe9a9173f3f6c4318040d88eb52bfc8
deephalder <subhradipmsm@gmail.com>
2020-02-14 16:56:01 +0100

    updating importdata.

--- R/import_data.R
+++ R/import_data.R
@@ -3,12 +3,13 @@
 #' @description This function reads the data from a csv-file and returns it as a DataFrame-object.It also handles missing age data using locf.
 #' @details This functions downloads the train.csv file from the package to the user's local and creates a dataframe.It then handles missing data and stores the dataframe to a .rda file under the data folder.
 #' @return This function returns the  dafaframe after its created.
+#' @import readr
 #' @examples import_data()
 #' @export
 import_data <- function() {
 
         #importing data from file.
-        df <- read.csv(system.file("extdata", "train.csv", package="MidDatAnalysis"))
+        df <- read_csv(system.file("extdata", "train.csv", package="MidDatAnalysis"))
 
 
         # as for this dataset we have a lot of missing values for the Age feature
