a0c0dfad9e7d94259b83f140fadee5120017dd0d
deephalder <subhradipmsm@gmail.com>
2020-02-14 17:12:28 +0100

    updating import

--- R/import_data.R
+++ R/import_data.R
@@ -3,22 +3,23 @@
 #' @description This function reads the data from a csv-file and returns it as a DataFrame-object.It also handles missing age data using locf.
 #' @details This functions downloads the train.csv file from the package to the user's local and creates a dataframe.It then handles missing data and stores the dataframe to a .rda file under the data folder.
 #' @return This function returns the  dafaframe after its created.
+#' @import readr
 #' @examples import_data()
 #' @export
 import_data <- function() {
 
         #importing data from file.
-        df <- readr::read_csv(system.file("extdata", "train.csv", package="MidDatAnalysis"))
+        df <- read_csv(system.file("extdata", "train.csv", package="MidDatAnalysis"))
 
-
-        # as for this dataset we have a lot of missing values for the Age feature
-        # we will be using the locf(last observation carried forward) function from Zoo package
-        # to fill the NA values.
-
-        df <- zoo::na.locf(df)
-
-        #saving df in a rda file,overwriting the file if already present:
-        usethis::use_data(df, overwrite = TRUE)
+#
+#         # as for this dataset we have a lot of missing values for the Age feature
+#         # we will be using the locf(last observation carried forward) function from Zoo package
+#         # to fill the NA values.
+#
+#         df <- zoo::na.locf(df)
+#
+#         #saving df in a rda file,overwriting the file if already present:
+#         usethis::use_data(df, overwrite = TRUE)
 
         #returning the main dataset df.
         return(df)
